65474ba52de017a12e70a327891acc0ac8d6b4f6
Ye Chan <yechan77@gmail.com>
2017-11-24 21:18:58 +0000

    Initial code

---
+++ p1a.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision
+import torchvision.dataset as dset
+from torch.utils.data import DataLoader,Dataset
+import torchvision.transforms as transforms
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import torchvision.utils
+import numpy as np
+import random
+from PIL import Image
+from torch.autograd import Variable
+import PIL.ImageOps
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
