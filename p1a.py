512ec66be0208a521e5c08b957156d1fe1882236
Ye Chan <yechan77@gmail.com>
2017-11-25 21:58:49 +0000

    Finished dataset and augmentation function

--- p1a.py
+++ p1a.py
@@ -16,6 +16,7 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 
+# read the training data and split each line
 file = open("train.txt", "r")
 temp = file.readlines()
 train = []
@@ -23,14 +24,38 @@ for i in range(len(temp)):
 	train.append(temp[i].split())
 
 def augmentations(image):
-	# Mirror flipping
-	#if random.random() < 0.5:
+	# Mirror flipping only left to right
+	if random.random() < 0.5:
 		image = image.transpose(Image.FLIP_LEFT_RIGHT)
 
-temp_img = Image.open('lfw/Aaron_Eckhart/Aaron_Eckhart_0001.jpg').convert('RGB')
-temp_img = augmentations(temp_img)
-cv2.imwrite('test.jpg', temp_img)
-'''
+	# Rotation between -30 to 30 degrees with respect to the center
+	if random.random() < 0.5:
+		rand_angle = random.randint(-30, 30)
+		image = image.rotate(rand_angle)
+
+	# Translation within -10 and +10 pixels
+	if random.random() < 0.5:
+		move_x = random.randint(-10, 10)
+		move_y = random.randint(-10, 10)
+		image = image.transform(image.size, Image.AFFINE, (1,0,move_x,0,1,move_y))	
+
+	# Scaling within 0.7 to 1.3
+	if random.random() < 0.5:
+		rand_ratio = 0.6 * random.random() + 0.7
+		og_size = image.size
+		new_size = tuple([int(i*rand_ratio) for i in image.size])
+		image = image.resize(new_size, Image.ANTIALIAS)
+		if rand_ratio <= 1:
+			image = image.crop((0,0,og_size[0],og_size[1]))
+		else:
+			left = abs((og_size[0] - new_size[0])/2)
+			right = abs((og_size[0] + new_size[0])/2)
+			top = abs((og_size[1] - new_size[1])/2)
+			bottom = abs((og_size[1] + new_size[1])/2)
+			image = image.crop((left,top,right,bottom))
+
+	return image
+
 class facesDataset(Dataset):
 
 	def __init__(self, train_data, img_path, transform=None, augment=False):
@@ -53,9 +78,11 @@ class facesDataset(Dataset):
 		prob_augment = (random.random() <= 0.7)
 		# it augments with a probability of 70%
 		if self.augment == True and prob_agument == True:
-					
-		
+			img1 = augmentations(img1)
+			img2 = augmentations(img2)
 
+		return img1, img2, label
+		
 	def __len__(self);
 		return len(self.train_data)
-'''
+
