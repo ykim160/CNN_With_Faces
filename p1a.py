39537875e38da52688790adb8d393b9b70c85862
Ye Chan <yechan77@gmail.com>
2017-11-25 18:14:34 +0000

    Progress

--- p1a.py
+++ p1a.py
@@ -1,6 +1,6 @@
 import torch
 import torchvision
-import torchvision.dataset as dset
+import torchvision.datasets as dset
 from torch.utils.data import DataLoader,Dataset
 import torchvision.transforms as transforms
 import torch.optim as optim
@@ -14,3 +14,25 @@ import PIL.ImageOps
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
+
+file = open("train.txt", "r")
+temp = file.readlines()
+train = []
+for i in range(len(temp)):
+	train.append(temp[i].split())
+
+class facesDataset(Dataset):
+
+	def __init__(self, train_data, img_path, transform=None):		
+		self.train_data = train_data
+		self.img_path = img_path
+		self.transform = transform
+
+	def __getitem(self, index):
+		img1_path = self.train_data[index][0]
+		img2_path = self.train_data[index][1]
+		img_label = torch.from_numpy(self.train_data[index][2])
+		img1 = Image.open(img1_path)
+		img1 = img1.convert('RGB')
+		img2 = Image.open(img2_path)
+		img2 = img2.convert('RGB')
