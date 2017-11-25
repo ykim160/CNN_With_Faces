247ffcbc9c7cc4a23d492f89afcf40c2bd627bb7
Ye Chan <yechan77@gmail.com>
2017-11-25 20:49:18 +0000

    Testing mirror flip

--- p1a.py
+++ p1a.py
@@ -1,4 +1,5 @@
 import torch
+import cv2
 import torchvision
 import torchvision.datasets as dset
 from torch.utils.data import DataLoader,Dataset
@@ -21,18 +22,40 @@ train = []
 for i in range(len(temp)):
 	train.append(temp[i].split())
 
+def augmentations(image):
+	# Mirror flipping
+	#if random.random() < 0.5:
+		image = image.transpose(Image.FLIP_LEFT_RIGHT)
+
+temp_img = Image.open('lfw/Aaron_Eckhart/Aaron_Eckhart_0001.jpg').convert('RGB')
+temp_img = augmentations(temp_img)
+cv2.imwrite('test.jpg', temp_img)
+'''
 class facesDataset(Dataset):
 
-	def __init__(self, train_data, img_path, transform=None):		
+	def __init__(self, train_data, img_path, transform=None, augment=False):
 		self.train_data = train_data
 		self.img_path = img_path
 		self.transform = transform
 
-	def __getitem(self, index):
-		img1_path = self.train_data[index][0]
-		img2_path = self.train_data[index][1]
-		img_label = torch.from_numpy(self.train_data[index][2])
-		img1 = Image.open(img1_path)
-		img1 = img1.convert('RGB')
-		img2 = Image.open(img2_path)
-		img2 = img2.convert('RGB')
+	def __getitem__(self, index):
+		img1_path = 'lfw/' + self.train_data[index][0]
+		img2_path = 'lfw/' + self.train_data[index][1]
+		img_label = self.train_data[index][2]
+		img1 = Image.open(img1_path).convert('RGB')
+		img2 = Image.open(img2_path).convert('RGB')
+
+		if self.transform is not None:
+			img1 = self.transform(img1)
+			img2 = self.transform(img2)
+
+		# if we need to augment it goes through this process
+		prob_augment = (random.random() <= 0.7)
+		# it augments with a probability of 70%
+		if self.augment == True and prob_agument == True:
+					
+		
+
+	def __len__(self);
+		return len(self.train_data)
+'''
